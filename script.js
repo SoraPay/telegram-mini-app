4a749d12403de2da2b947c5638823124743c812d
SoraPay <laichuks@yandex.ru>
2025-05-14 18:27:40 +0300

    next 12

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
 const textInput = document.getElementById('textInput');
 const webApp = window.Telegram.WebApp;
 
-console.log('Telegram.WebApp:', webApp);
 webApp.ready();
-console.log('webApp.ready() выполнен');
 
 function sendText() {
     const text = textInput.value.trim();
-    console.log('Текст:', text);
     if (!text) {
         webApp.showAlert('Введи текст!');
         return;
     }
-    const botCommand = `https://t.me/testShopFood_bot?start=webapp_data_${encodeURIComponent(text)}`;
-    console.log('Команда для бота:', botCommand);
-    try {
-        webApp.openLink(botCommand); // Отправляем команду через ссылку
-        webApp.showAlert('Команда отправлена: ' + botCommand);
-        setTimeout(() => webApp.close(), 500);
-    } catch (error) {
-        webApp.showAlert('Ошибка: ' + error.message);
-    }
+    webApp.sendData(text); // Отправляем текст напрямую
+    setTimeout(() => webApp.close(), 1000); // Задержка 1с
 }
